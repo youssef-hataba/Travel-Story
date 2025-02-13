0daa06191cbd9f858eb14fa575e0cbd08a1ab623
youssef hataba <joohataba@gmail.com>
2025-02-13 23:47:40 +0200

    Sign Up Page

--- frontend/travel-story/src/pages/Auth/SignUp.jsx
+++ frontend/travel-story/src/pages/Auth/SignUp.jsx
@@ -1,9 +1,125 @@
+import {useNavigate} from "react-router";
+import PasswordInput from "../../components/Input/PasswordInput";
+import {useState} from "react";
+import {validateEmail} from "../../utils/helper";
+import axiosInstance from "../../utils/axiosInstance";
 const SignUp = () => {
-  return (
-    <div>
-      SignUp Page
-    </div>
-  )
-}
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
 
-export default SignUp
+  const navigate = useNavigate();
+
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+
+    if (!name) {
+      setError("Please enter your name");
+      return;
+    }
+    if (!validateEmail(email)) {
+      setError("Please enter a valid email address");
+      return;
+    }
+
+    if (!password) {
+      setError("Please enter a password");
+      return;
+    }
+
+    setError("");
+
+
+    //Login API Call 
+    try{
+      const response = await axiosInstance.post("/signUp", {
+        fullName:name,
+        email,
+        password,
+      });
+      
+      if(response.data && response.data.accessToken){
+        localStorage.setItem("token", response.data.accessToken);
+        navigate("/dashboard");
+      }
+    }catch(error){
+      if(error.response && error.response.data && error.response.data.message){
+        setError(error.response.data.message);
+      }else{
+        setError("An error occurred while trying to SignUp. Please try again");
+      }
+    }
+  };
+
+  return (
+    <div className="h-screen bg-cyan-50 overflow-hidden relative">
+      <div className="login-ui-box right-10 -top-40"></div>
+      <div className="login-ui-box bg-cyan-200 -bottom-40 right-1/2"></div>
+
+      <div className="container h-screen flex items-center justify-center px-20 w-[80%] mx-auto">
+        <div className="w-2/4 h-[90vh] flex items-end signup-bg-img bg-cover bg-center rounded-lg p-10 z-50">
+          <div>
+            <h4 className="text-5xl text-cyan-100 font-semibold leading-[58px]">
+              Join the <br />Adventure
+            </h4>
+            <p className="text-[15px] text-cyan-200 leading-6 pr-7 mt-4">
+              Create an account to start documenting your travels and preserving 
+              your memories in your personal travel journal.
+            </p>
+          </div>
+        </div>
+
+        <div className="w-2/4 h-[75vh] bg-white rounded-r-lg relative p-16 shadow-lg shadow-cyan-200/40">
+          <form onSubmit={handleSignUp}>
+            <h4 className="text-2xl font-semibold mb-7">SignUP</h4>
+
+            <input
+              type="text"
+              placeholder="Full Name"
+              className="input-box"
+              value={name}
+              onChange={({target}) => {
+                setName(target.value);
+              }}
+            />
+
+            <input
+              type="text"
+              placeholder="Email"
+              className="input-box"
+              value={email}
+              onChange={({target}) => {
+                setEmail(target.value);
+              }}
+            />
+
+            <PasswordInput
+              value={password}
+              onChange={({target}) => {
+                setPassword(target.value);
+              }}
+            />
+
+            {error && <p className="text-red-500 text-xs pb-1">{error}</p>}
+
+            <button type="submit" className="btn-primary">
+              CREATE ACCOUNT
+            </button>
+            <p className="text-xs text-slate-500 text-center my-4">Or</p>
+            <button
+              type="submit"
+              className="btn-primary btn-light"
+              onClick={() => {
+                navigate("/login");
+              }}>
+              LOGIN
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SignUp;
